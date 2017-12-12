a10d4580e2967b84a7c1c9f642e54ae3d5616cc7
NRB <nielsrb@outlook.dk>
2017-12-12 12:21:21 +0100

    V0.04 - Abstract Class Opgave - Add name and foreach loop to main

--- AbstractOpgaveFigur/AbstractOpgaveFigur/Program.cs
+++ AbstractOpgaveFigur/AbstractOpgaveFigur/Program.cs
@@ -10,25 +10,46 @@ namespace AbstractOpgaveFigur
     {
         static void Main(string[] args)
         {
-            Kvadrat k1 = new Kvadrat(5);
+            Kvadrat k1 = new Kvadrat("kvadrat1", 5);
+            Kvadrat k2 = new Kvadrat("kvadrat2", 10);
             k1.SetAreal(k1.GetSize());
-            Cirkel C1 = new Cirkel(1);
+            k2.SetAreal(k2.GetSize());
+            Cirkel C1 = new Cirkel("cirkel1", 1);
             C1.SetAreal(C1.GetSize());
             Console.WriteLine(k1.ToString());
             Console.WriteLine(C1.ToString());
+
+            List<Kvadrat> list = new List<Kvadrat>();
+            list.Add(k1);
+            list.Add(k2);
+
+            foreach (Kvadrat item in list)
+            {
+                Console.WriteLine(item.ToString());
+            }
+
         }
     }
     abstract class Figur
     {
         //fields
+        private string _name;
         private double _size;
-        protected double _Areal;
+        protected double _areal;
 
         //abstract methods
         abstract public double GetAreal();
         abstract public void SetAreal(double s);
 
         //methods
+        public string GetName()
+        {
+            return _name;
+        }
+        public void SetName(string n)
+        {
+            _name = n;
+        }
         public double GetSize()
         {
             return _size;
@@ -39,20 +60,21 @@ namespace AbstractOpgaveFigur
         }
 
         //constructor
-        public Figur(double s)
+        public Figur(string n, double s)
         {
+            SetName(n);
             SetSize(s);
         }
         //override
         public override string ToString()
         {
-            return string.Format("Size er {0} og Arealet er {1}", GetSize(), GetAreal());
+            return string.Format("Navn er {0}, Size er {1} og Arealet er {2}", GetName(), GetSize(), GetAreal());
         }
 
     }
     class Kvadrat : Figur
     {
-        public Kvadrat(double s) : base(s)
+        public Kvadrat(string n,double s) : base(n,s)
         {
 
         }
@@ -60,30 +82,30 @@ namespace AbstractOpgaveFigur
         //override
         public override double GetAreal()
         {
-            return _Areal; 
+            return _areal; 
         }
         public override void SetAreal(double s)
         {
-            _Areal = s * s;
+            _areal = s * s;
         }
 
     }
     class Cirkel : Figur
     {
         //Constructor
-        public Cirkel(double s) : base(s)
+        public Cirkel(string n, double s) : base(n,s)
         {
 
         }
         //Override
         public override double GetAreal()
         {
-            return _Areal;
+            return _areal;
         }
 
         public override void SetAreal(double s)
         {
-            _Areal = Math.Pow(GetSize(), 2) * Math.PI;
+            _areal = Math.Pow(GetSize(), 2) * Math.PI;
         }
     }
 }
