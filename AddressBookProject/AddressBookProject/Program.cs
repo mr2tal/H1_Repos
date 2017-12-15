e86569ba074847088bde81055b3efe817efb9e22
NRB <nielsrb@outlook.dk>
2017-12-15 10:40:44 +0100

    V0.13 - Exam , MyPersonClass & AddressBookProject

---
+++ AddressBookProject/AddressBookProject/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AddressBook jan = new AddressBook("Jan");
+            Address a1 = new Address("Hans Peter Nielsen", "Birkehøj 5", "2134123456");
+            Address a2 = new Address("Jens Peter Larsen", "Poppelhøj 6", "2345619481");
+            jan.AddAddress(a1);
+            jan.AddAddress(a2);
+            
+
+
+            AddressBook bo = new AddressBook("Bo");
+            Address a3 = new Address("Niels Peter Hansen", "Bøgehøj 7", "213412345636");
+            Address a4 = new Address("Lars Peter Jensen", "Fyrrehøj", "2345679801");
+            bo.AddAddress(a3);
+            bo.AddAddress(a4);
+            
+            Console.WriteLine(jan);
+            Console.WriteLine(bo);
+
+            AddressBook Jan_and_Bo = jan + bo;
+
+            Console.WriteLine(Jan_and_Bo);
+
+            Console.ReadKey();
+
+
+        }
+    }
+    class Address
+    {
+        //fields
+        private string _Name;
+        private string _StreetAddress;
+        private string _Telephone;
+
+        //properties
+        public string Name
+        {
+            get { return _Name; }
+            set { _Name = value; }
+        }
+        public string StreetAddress
+        {
+            get { return _StreetAddress; }
+            set { _StreetAddress = value; }
+        }
+        public string Telephone
+        {
+            get { return _Telephone; }
+            set { _Telephone = value; }
+        }
+
+        //Constructor
+        public Address(string n, string sa, string t)
+        {
+            Name = n;
+            StreetAddress = sa;
+            Telephone = t;
+        }
+
+        //Methods
+        public override string ToString()
+        {
+            return string.Format("Name {0}, Street address: {1}, Telephone: {2}", Name, StreetAddress, Telephone);
+        }
+    }
+    class AddressBook
+    {
+        //fields
+        private string _Owner;
+        List<Address> _Addresses = new List<Address>();
+
+        //Properties
+        public string Owner
+        {
+            get { return _Owner; }
+            set { _Owner = value; }
+        }
+        //Constructor
+        public AddressBook(string o)
+        {
+            Owner = o;
+        }
+        //Methods
+        public void AddAddress(Address a)
+        {
+            _Addresses.Add(a);
+        }
+        public override string ToString()
+        {
+            string n = string.Format("Owner: {0}\n",Owner);
+            foreach(Address item in _Addresses)
+            {
+               n = n + string.Format("Name :{0}, Street Address: {1},Telephone: {2}\n", item.Name, item.StreetAddress,item.Telephone);
+            }
+            return n;
+        }
+        public static AddressBook operator +(AddressBook x, AddressBook y)
+        {
+            AddressBook ab1 = new AddressBook("");
+            ab1.Owner = string.Format("{0} and {1}", x.Owner, y.Owner);
+            
+            ab1.AddAddress(x._Addresses[0]);
+            ab1.AddAddress(x._Addresses[1]);
+            ab1.AddAddress(y._Addresses[0]);
+            ab1.AddAddress(y._Addresses[1]);
+
+            return ab1;
+            
+        }
+    }
+}
---
+++ MyPersonClass/MyPersonClass/Program.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyPersonClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> persons = new List<Person>();
+            Person p1 = new Person("Niels", 120, 1.85);
+            Person p2 = new Person();
+            p2.Name = "Joakim";
+            p2.Weight = 80;
+            p2.Height = 1.88;
+            Person p3 = new Person("Mark", 75, 1.83);
+
+            Console.WriteLine("Print persons");
+            Console.WriteLine(p1);
+            Console.WriteLine(p2);
+            Console.WriteLine(p3);
+            
+            persons.Add(p1);
+            persons.Add(p2);
+            persons.Add(p3);
+
+            persons.Sort();
+            WriteList("Sorted by Name", persons);
+            persons.Sort(new CompareWeight());
+            WriteList("Sorted by Weight", persons);
+            persons.Sort(new CompareHeight());
+            WriteList("Sorted by Height", persons);
+            persons.Sort(new CompareBMI());
+            WriteList("Sorted by BMI", persons);
+
+            Console.ReadKey();
+
+
+
+        }
+        public static void WriteList(string s, List<Person> list)
+        {
+            Console.WriteLine(s);
+            foreach (Person item in list)
+            {
+                Console.WriteLine(item.ToString());
+            }
+        }
+    }
+
+    class Person : IComparable
+    {
+        //Fields
+        private string _Name;
+        private int _Weight;
+        private double _Height;
+
+        //Properties
+
+        public string Name
+        {
+            get { return _Name; }
+            set { _Name = value; }
+        }
+
+        public int Weight
+        {
+            get { return _Weight; }
+            set { _Weight = value; }
+        }
+
+        public double Height
+        {
+            get { return _Height; }
+            set { _Height = value; }
+        }
+
+        public double BMI
+        {
+            get { return Weight / (Math.Pow(Height, 2)); }
+        }
+
+        //Constructors
+        public Person()
+        {
+
+        }
+        public Person(string n, int w, double h)
+        {
+            Name = n;
+            Weight = w;
+            Height = h;
+        }
+        
+        //Methods
+        public double GetBMI()
+        {
+            double result = Weight / (Math.Pow(Height, 2));
+            return result;
+        }
+        public override string ToString()
+        {
+            return string.Format("Name: {0}, Weight: {1}, Height: {2}, BMI: {3}", Name, Weight, Height, GetBMI());
+        }
+        public int CompareTo(object obj)
+        {
+            return this.Name.CompareTo(((Person)obj).Name);
+        }
+
+    }
+    class CompareWeight : IComparer<Person>
+    {
+        public int Compare(Person x, Person y)
+        {
+            return x.Weight.CompareTo(y.Weight);
+        }
+    }
+    class CompareHeight : IComparer<Person>
+    {
+        public int Compare(Person x, Person y)
+        {
+            return x.Height.CompareTo(y.Height);
+        }
+    }
+    class CompareBMI : IComparer<Person>
+    {
+        public int Compare(Person x, Person y)
+        {
+            return x.GetBMI().CompareTo(y.GetBMI());
+        }
+    }
+
+}
